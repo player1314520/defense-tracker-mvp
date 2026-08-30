ad815b6fc4acbfb1ac22451a8eb064bb2f390aab
player1314520 <168609221+player1314520@users.noreply.github.com>
2026-08-30 15:52:49 +0800

    Test null smoke responses under strict mode

--- scripts/Build-AndShip.ps1
+++ scripts/Build-AndShip.ps1
@@ -506,6 +506,29 @@ function Invoke-SignAndVerify {
     }
 }
 
+function Get-SmokeTransportStatus {
+    param([Parameter(Mandatory = $true)]$ErrorRecord)
+    $responseValues = @(
+        $ErrorRecord.Exception.PSObject.Properties |
+            Where-Object { $_.Name -eq "Response" } |
+            ForEach-Object { $_.Value } |
+            Where-Object { $null -ne $_ }
+    )
+    $statusValues = @(
+        $responseValues |
+            ForEach-Object {
+                $_.PSObject.Properties |
+                    Where-Object { $_.Name -eq "StatusCode" } |
+                    ForEach-Object { $_.Value } |
+                    Where-Object { $null -ne $_ }
+            }
+    )
+    if ($statusValues.Count -eq 1) {
+        return "http-" + [int]$statusValues[0]
+    }
+    return "connection-error"
+}
+
 function Invoke-DesktopSmokeTest {
     param(
         [Parameter(Mandatory = $true)][string]$ExePath,
@@ -575,24 +598,7 @@ function Invoke-DesktopSmokeTest {
                             -Method Get -TimeoutSec 1 -ErrorAction Stop
                         $lastTransportStatus = "http-200"
                     } catch {
-                        $responseValues = @(
-                            $_.Exception.PSObject.Properties |
-                                Where-Object { $_.Name -eq "Response" } |
-                                ForEach-Object { $_.Value }
-                        )
-                        $statusValues = @(
-                            $responseValues |
-                                ForEach-Object {
-                                    $_.PSObject.Properties |
-                                        Where-Object { $_.Name -eq "StatusCode" } |
-                                        ForEach-Object { $_.Value }
-                                }
-                        )
-                        if ($statusValues.Count -eq 1) {
-                            $lastTransportStatus = "http-" + [int]$statusValues[0]
-                        } else {
-                            $lastTransportStatus = "connection-error"
-                        }
+                        $lastTransportStatus = Get-SmokeTransportStatus -ErrorRecord $_
                     }
                     if ($null -ne $response) {
                         $evidence = $response.evidence
--- scripts/Finalize-SignedCandidate.ps1
+++ scripts/Finalize-SignedCandidate.ps1
@@ -381,6 +381,29 @@ function Get-ArtifactSafetyFindings {
     return @($findings)
 }
 
+function Get-SmokeTransportStatus {
+    param([Parameter(Mandatory = $true)]$ErrorRecord)
+    $responseValues = @(
+        $ErrorRecord.Exception.PSObject.Properties |
+            Where-Object { $_.Name -eq 'Response' } |
+            ForEach-Object { $_.Value } |
+            Where-Object { $null -ne $_ }
+    )
+    $statusValues = @(
+        $responseValues |
+            ForEach-Object {
+                $_.PSObject.Properties |
+                    Where-Object { $_.Name -eq 'StatusCode' } |
+                    ForEach-Object { $_.Value } |
+                    Where-Object { $null -ne $_ }
+            }
+    )
+    if ($statusValues.Count -eq 1) {
+        return 'http-' + [int]$statusValues[0]
+    }
+    return 'connection-error'
+}
+
 function Invoke-DesktopSmokeTest {
     param([string]$ExePath,[string]$RuntimeRoot,$Version,[string]$ExpectedCommit)
     $previousHome = [Environment]::GetEnvironmentVariable('DEFENSE_TRACKER_HOME','Process')
@@ -443,24 +466,7 @@ function Invoke-DesktopSmokeTest {
                             -Method Get -TimeoutSec 1 -ErrorAction Stop
                         $lastTransportStatus = 'http-200'
                     } catch {
-                        $responseValues = @(
-                            $_.Exception.PSObject.Properties |
-                                Where-Object { $_.Name -eq 'Response' } |
-                                ForEach-Object { $_.Value }
-                        )
-                        $statusValues = @(
-                            $responseValues |
-                                ForEach-Object {
-                                    $_.PSObject.Properties |
-                                        Where-Object { $_.Name -eq 'StatusCode' } |
-                                        ForEach-Object { $_.Value }
-                                }
-                        )
-                        if ($statusValues.Count -eq 1) {
-                            $lastTransportStatus = 'http-' + [int]$statusValues[0]
-                        } else {
-                            $lastTransportStatus = 'connection-error'
-                        }
+                        $lastTransportStatus = Get-SmokeTransportStatus -ErrorRecord $_
                     }
                     if ($null -ne $response) {
                         $evidence = $response.evidence
